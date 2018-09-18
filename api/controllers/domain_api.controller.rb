05e47a0ed61e370e53e31f54a21477581f122625
Victor Vu <victor.vinh.vu@gmail.com>
2018-09-18 16:16:11 -0700

    Added domain deletion

--- api/controllers/domain_api.controller.rb
+++ api/controllers/domain_api.controller.rb
@@ -3,7 +3,7 @@ controller :domains do
     description "This API allows you to access message details"
     authenticator :server
   
-    action :check do
+    action :verify do
       title "Returns verification information about a domain"
       description "Returns the verification about a domain"
       param :domain, "The domain that you're checking", :type => String, :required => true
@@ -24,7 +24,7 @@ controller :domains do
     action :add do
       title "Adds a domain"
       description "Adds a domain to the mail server"
-      param :domain, "The ID of the message", :type => String, :required => true
+      param :domain, "The domain to add", :type => String, :required => true
       returns Hash, :structure => :domain, :structure_opts => {:paramable => {:expansions => false}}
       error 'CouldNotAddDomain', "Could not add domain", :attributes => {:domain => "The domain to add."}
       action do
@@ -38,6 +38,25 @@ controller :domains do
           domain.save!
           domain.check_dns
           structure :domain, domain, :return => true
+        end
+      end
+    end
+
+    action :delete do
+      title "Deletes a domain"
+      description "Deletes a domain on the mail server"
+      param :domain, "The domain to delete", :type => String, :required => true
+      returns Hash, :structure => :domain, :structure_opts => {:paramable => {:expansions => false}}
+      error 'DomainNotFound', "No matching domain found", :attributes => {:domain => "The domain to verify."}
+      error 'CouldNotDeleteDomain', "Could not delete domain", :attributes => {:domain => "The domain to delete."}
+      action do
+        begin
+          domain = Domain.find_by_name(params.domain)
+          if domain.nil?
+            error 'DomainNotFound', :id => params.domain
+          end
+          domain.destroy
+          { "deleted" => domain.destroyed? }
         end
       end
     end
